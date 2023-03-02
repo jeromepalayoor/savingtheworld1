a8d14a7e0ca8f99a2bc1af75c3b4669d191d18f3
jeromepalayoor <me@jeromepalayoor.com>
2023-03-02 14:07:45 +0800

    added index.html and updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-import flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        return(f'Hello, {name}')
+    return render_template('index.html')
