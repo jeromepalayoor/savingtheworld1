66c5afe3aded17c752ca1e08d00a7ed9a663d58e
jeromepalayoor <me@jeromepalayoor.com>
2023-03-02 14:53:44 +0800

    what

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-from flask import Flask, render_template
-from markupsafe import escape
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    if request.method == 'POST':
-        name = request.form['name']
-        return(f'Hello, {escape(name)}')
-    return render_template('index.html')
+    return render_template('/templates/index.html')
