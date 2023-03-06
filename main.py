ad739b10c98084fdb0c54cef28fa309517fe2134
jeromepalayoor <me@jeromepalayoor.com>
2023-03-06 11:56:21 +0800

    idk

--- main.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET','POST'])
-def index():
-    return render_template('/templates/index.html')
