f6f67acad2cab3783876893b678a59e5a4dec6d4
jeromepalayoor <me@jeromepalayoor.com>
2023-03-06 12:12:47 +0800

    yes

--- view.py
+++ view.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/name')
+def name():
+    return "hi jerome"
+
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
