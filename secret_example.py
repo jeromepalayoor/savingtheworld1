f110de4203f436e2fa3288086de1df1a6ed5bf27
jeromepalayoor <me@jp-ch.gq>
2023-03-15 00:48:25 +0800

    'updated stuff'

---
+++ secret_example.py
@@ -0,0 +1,3 @@
+myemail = ''  #email address with which you wish to send verification emails
+mypassword = ''   #get an app password from google account -> security -> app password -> put any custom name -> get that password put here
+salt = ''      #create a string of characters that is preferably random then do not change it after setting it!
