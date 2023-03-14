59b512d30d23c2cec48920048af37cff8fb25733
jeromepalayoor <me@jp-ch.gq>
2023-03-15 00:44:43 +0800

    finally can go sleep after fixing session token storage issues

--- app.py
+++ app.py
@@ -13,23 +13,26 @@ alphanumeric = string.ascii_letters + string.digits
 
 app = Flask(__name__)
 
-#home page, to update
+
+# home page, to update
 @app.route("/")
 def home():
     return render_template("index.html")
 
-#send emails to verify account
+
+# send emails to verify account
 def send_email(subject, body, sender, recipients, password):
     msg = MIMEText(body)
-    msg['Subject'] = subject
-    msg['From'] = "Savingtheworld1 Account Verification"
-    msg['To'] = ', '.join(recipients)
-    smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    msg["Subject"] = subject
+    msg["From"] = "Savingtheworld1 Account Verification"
+    msg["To"] = ", ".join(recipients)
+    smtp_server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
     smtp_server.login(sender, password)
     smtp_server.sendmail(sender, recipients, msg.as_string())
     smtp_server.quit()
 
-#login handler
+
+# login handler
 @app.route("/login", methods=["POST", "GET"])
 def login():
     # checks whether user is logged in arld
@@ -46,7 +49,11 @@ def login():
     # logs in if username and password is correct
     if request.method == "POST":
         username = request.form["username"].strip()
-        password = str(sha256(str(request.form["password"].strip() + salt).encode("utf-8")).hexdigest())
+        password = str(
+            sha256(
+                str(request.form["password"].strip() + salt).encode("utf-8")
+            ).hexdigest()
+        )
         data = []
         with open("db/users", "r") as f:
             for i in f.read().strip().splitlines():
@@ -57,22 +64,29 @@ def login():
                 cookies = []
                 with open("db/sessions", "r") as f:
                     a = f.read().strip().splitlines()
-                    cookies.append(a.split(","))
+                    for b in a:
+                        cookies.append(b.split(","))
                 with open("db/sessions", "w") as f:
                     adding = ""
                     for c in cookies:
+                        print(c)
+                        print(username)
+                        print(c[0] == username)
                         if c[0] != username:
-                            adding += f"{a[0]},{a[1]}\n"
-                    adding += f'{username},{cookie}\n'
+                            adding += f"{c[0]},{c[1]}\n"
+                    adding += f"{username},{cookie}\n"
+                    f.write(adding)
                 resp = make_response(redirect("/users/" + username))
                 resp.set_cookie("user", cookie)
                 return resp
-        return make_response(redirect("/error?error=Incorrect username and/or password"))
+        return make_response(
+            redirect("/error?error=Incorrect username and/or password")
+        )
     else:
         return render_template("login.html")
 
 
-#register new account
+# register new account
 @app.route("/register", methods=["POST", "GET"])
 def register():
     # checks whether user is logged in arld
@@ -89,14 +103,20 @@ def register():
     if request.method == "POST":
         username = request.form["username"].strip().replace("\n", " ").replace(",", " ")
         if not username.isalnum():
-            return make_response(redirect("/error?error=Username contains invalid characters"))
+            return make_response(
+                redirect("/error?error=Username contains invalid characters")
+            )
         email = request.form["email"].strip().replace("\n", " ").replace(",", " ")
         password = request.form["password"].strip().replace("\n", " ").replace(",", " ")
         if not password.isalnum():
-            return make_response(redirect("/error?error=Password contains invalid characters"))
+            return make_response(
+                redirect("/error?error=Password contains invalid characters")
+            )
         class_ = request.form["class"].strip().replace("\n", " ").replace(",", " ")
         fullname = request.form["fullname"].strip().replace("\n", " ").replace(",", " ")
-        if not re.search("^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[-]?\w+[.]\w{2,3}$", email): #check if email is valid using regex
+        if not re.search(
+            "^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[-]?\w+[.]\w{2,3}$", email
+        ):  # check if email is valid using regex
             return make_response(redirect("/error?error=Email is invalid"))
         data = []
         with open("db/users", "r") as f:
@@ -105,30 +125,52 @@ def register():
         for d in data:
             print(d)
             if username == d[0]:
-                return make_response(redirect("/error?error=Username is in use already"))
+                return make_response(
+                    redirect("/error?error=Username is in use already")
+                )
             if email == d[3]:
                 return make_response(redirect("/error?error=Email is in use already"))
-        with open('db/users', 'a') as f:
-            adding = username + ',' + str(sha256(str(password + salt).encode("utf-8")).hexdigest()) + ',' + fullname + ',' + email + ',' + class_ + ',' + 'False' + '\n'
+        with open("db/users", "a") as f:
+            adding = (
+                username
+                + ","
+                + str(sha256(str(password + salt).encode("utf-8")).hexdigest())
+                + ","
+                + fullname
+                + ","
+                + email
+                + ","
+                + class_
+                + ","
+                + "False"
+                + "\n"
+            )
             f.write(adding)
-        token = str(uuid.uuid4())+str(uuid.uuid4())+str(uuid.uuid4())
+        token = str(uuid.uuid4()) + str(uuid.uuid4()) + str(uuid.uuid4())
         with open("db/verification", "a") as f:
             f.write(username + "," + token + "\n")
-        send_email("Blog account activated", f"Please verify your email before using your account {request.url_root}verify?token={token}", myemail, [email], mypassword)
+        send_email(
+            "Blog account activated",
+            f"Please verify your email before using your account {request.url_root}verify?token={token}",
+            myemail,
+            [email],
+            mypassword,
+        )
         cookie = str(uuid.uuid4())
         with open("db/sessions", "a") as f:
             f.write(username + "," + cookie + "\n")
-        resp = make_response(redirect("/users/" + username))
+        resp = make_response(redirect("/verify"))
         resp.set_cookie("user", cookie)
         return resp
     else:
         return render_template("register.html")
 
-#verify email
+
+# verify email
 @app.route("/verify")
 def verify():
-    if request.args.get('token'):
-        token = request.args.get('token')
+    if request.args.get("token"):
+        token = request.args.get("token")
         data = []
         with open("db/verification", "r") as f:
             for i in f.read().strip().splitlines():
@@ -144,16 +186,21 @@ def verify():
                         user[5] = "True"
                 with open("db/users", "w") as f:
                     for user in userdata:
-                        f.write(f"{user[0]},{user[1]},{user[2]},{user[3]},{user[4]},{user[5]}\n")
+                        f.write(
+                            f"{user[0]},{user[1]},{user[2]},{user[3]},{user[4]},{user[5]}\n"
+                        )
                 with open("db/verification", "w") as f:
                     for d in data:
                         if d[1] != token:
-                            f.write(f'{d[0]},{d[1]}\n')
+                            f.write(f"{d[0]},{d[1]}\n")
                 return render_template("verified.html")
-        return make_response(redirect("/error?error=Verification link is either invalid or has expired"))
+        return make_response(
+            redirect("/error?error=Verification link is either invalid or has expired")
+        )
     return render_template("verify.html")
 
-#user page
+
+# user page
 @app.route("/users/<username>")
 def userpage(username):
     data = []
@@ -162,18 +209,20 @@ def userpage(username):
             data.append(i.split(","))
     for d in data:
         if d[0] == username:
-            return f'Username: {d[0]}<br>Full name: {d[2]}<br>Class: {d[4]}<br>Verified: {d[5]}'
+            return f"Username: {d[0]}<br>Full name: {d[2]}<br>Class: {d[4]}<br>Verified: {d[5]}"
     return f"User '{username}' not found"
-        
-#error handling
+
+
+# error handling
 @app.route("/error")
 def error():
-    if request.args.get('error'):
-        return request.args.get('error')
+    if request.args.get("error"):
+        return request.args.get("error")
     resp = make_response(redirect("/"))
     return resp
 
-#404 page not found error handling
+
+# 404 page not found error handling
 @app.errorhandler(404)
 def page_not_found(e):
     return "this page doesnt exist", 404
